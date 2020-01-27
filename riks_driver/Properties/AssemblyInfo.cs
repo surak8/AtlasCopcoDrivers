fcba458d4dac989989dd8ad638e4a82a21a506f2
Rik Cousens <rik.cousens@gmail.com>
2020-01-27 18:29:36 -0500

    initial deposit

---
+++ riks_driver/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly:AssemblyTitle("riks_driver")]
+[assembly:AssemblyProduct("riks_driver")]
+[assembly:AssemblyDescription("description of riks_driver.")]
+[assembly:AssemblyCompany("Rik Cousens")]
+[assembly:AssemblyCopyright("Copyright © 2020, Rik Cousens")]
+#if DEBUG
+[assembly:AssemblyConfiguration("Debug version")]
+#else
+[assembly:AssemblyConfiguration("Release version")]
+#endif
+[assembly:ComVisible(false)]
+
+[assembly:AssemblyVersion("1.0.0.0")]
+
